23a6728227b8950d6e6a2576d4db9307edcf0ee4
Nicolas Earnshaw <earnshaw.nico@gmail.com>
2013-11-19 01:11:10 -0300

    first real commit
    basically works... very buggy and missing storyline but works

---
+++ otros tests/wallobj.js
@@ -0,0 +1,39 @@
+{
+	"metadata": {
+		"version": 4.3,
+		"type": "Object",
+		"generator": "ObjectExporter"
+	},
+	"geometries": [
+		{
+			"uuid": "B133FC98-2D13-41A0-824D-3633CFF213B6",
+			"type": "CubeGeometry",
+			"width": 100,
+			"height": 100,
+			"depth": 100,
+			"widthSegments": 1,
+			"heightSegments": 1,
+			"depthSegments": 1
+		}],
+	"materials": [
+		{
+			"uuid": "5A2C46DF-B61F-4683-A3F2-D9A5D5E7E188",
+			"type": "MeshPhongMaterial",
+			"color": 16777215,
+			"ambient": 16777215,
+			"emissive": 0,
+			"specular": 1118481,
+			"shininess": 30,
+			"opacity": 1,
+			"transparent": false,
+			"wireframe": false
+		}],
+	"object": {
+		"uuid": "20846E77-AD04-4CEE-9532-2BC03B3CE0E7",
+		"name": "wall1",
+		"type": "Mesh",
+		"geometry": "B133FC98-2D13-41A0-824D-3633CFF213B6",
+		"material": "5A2C46DF-B61F-4683-A3F2-D9A5D5E7E188",
+		"matrix": [9.22350788116455,0,0,0,0,2.4413039684295654,0,0,0,0,0.08039166778326035,0,108.25814056396484,30.887340545654297,52.97016525268555,1]
+	}
+}
